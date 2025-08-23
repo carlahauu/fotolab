dfb57a7701922a7835ccb7060780a21637d489c2
carlahauu <carlahau8@gmail.com>
2025-08-23 02:23:20 -0700

    Redesigned landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,23 +5,21 @@ function Landing() {
   return (
     <div className='landingContainer'>
         <div className="hero">
-            <h1>Bring the photo booth experience straight to your device!</h1>
-            <p>An online photo booth that lets you capture and download your own photo strips in seconds!</p>
-            <a href='/photobooth'><button>Try for free!</button></a>
+            <div className="heroImg">
+                <img src='/Hero Image.svg'></img>
+            </div>
+            <div className="heroRight">
+                <h1>Bring the photo booth experience straight to your device!</h1>
+                <p>An online photo booth that lets you capture and download your own photo strips in seconds!</p>
+                <a href='/photobooth'><button>Try for free!</button></a>
+            </div>
         </div>
         <div className="steps">
             <h1>Create photo strips in 3 easy steps.</h1>
-            <div className="step">
-                <h3>01</h3>
-                <p>Select a frame for your photo strip!</p>
-            </div>
-            <div className="step">
-                <h3>02</h3>
-                <p>Take 8 shots, then choose 4 to create your perfect photo strip.</p>
-            </div>
-            <div className="step">
-                <h3>03</h3>
-                <p>Download your photo strip! Share it with friends! </p>
+            <div className="stepImages">
+                <img src='/Step 1.svg'></img>
+                <img src='/Step 2.svg'></img>
+                <img src='/Step 3.svg'></img>
             </div>
         </div>
         <div className="cta">
