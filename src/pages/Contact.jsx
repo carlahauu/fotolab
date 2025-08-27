64b6d4f4808f5c9cac542861299e2d94872a82a3
carlahauu <carlahau8@gmail.com>
2025-08-26 23:56:12 -0700

    Updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styles/Contact.css';
 
 function Contact() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+    formData.append("access_key", import.meta.env.VITE_WEBFORM_EMAIL_API_KEY);
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully!");
+        event.target.reset();
+      } else {
+        console.error("Error", data);
+        setResult(data.message || "Something went wrong!");
+      }
+    } catch (error) {
+      console.error(error);
+      setResult("Submission failed. Please try again.");
+    }
+  };
+
   return (
-    <div className='privacyContainer'>
-        <h1>Contact</h1>
-        <p>Got any questions about Fotolab? Feel free to contact: carlahau110@gmail.com</p>
+    <div className='contactContainer'>
+      <div className="topBlur"></div>
+      <div className="bottomBlur"></div>
+      <h1>Let's get in touch!</h1>
+      <p>
+        Got any questions about Fotolab?
+        <br></br>
+        Feel free to email: carlahau110@gmail.com
+      </p>
+
+      {result && result !== "Sending...." ? (
+        <>
+          <p>{result}</p>
+          <p>Thanks for reaching out! I'll get back to you as soon as I can!</p>
+        </>
+      ) : result === "Sending...." ? (
+          <span>Sending...</span>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <div className="inputs">
+            <input type="text" name="name" placeholder='Your Name' required />
+            <input type="email" name="email" placeholder='Your Email' required />
+            <textarea name="message" placeholder='Your Message' required></textarea>
+          </div>
+          <button type="submit">Submit Form</button>
+        </form>
+      )}
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
